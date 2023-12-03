2751eafea124068ed8fbf1d5f3e211141c4344a1
Aleena Kurumunda <kurumunda.a@northeastern.edu>
2023-12-03 14:56:45 -0500

    convert images to uint16, and recast labels so that there are 2 categories

--- hypso_data/convert2tifs.py
+++ hypso_data/convert2tifs.py
@@ -3,27 +3,43 @@ import numpy as np
 import sys
 import glob
 import os
+from sklearn.preprocessing import MinMaxScaler
+
+# This is for converting images to tifs, not the ground truth labels.
 
 SEAMAP_PATH = "C:/Users/akuru/Documents/ECEE 7370 Advanced Comp Vision/sea_cloud_land/1-DATA WITH GROUND-TRUTH LABELS"
 
-
-
 products = [
         x
-        for x in glob.glob(os.path.join(SEAMAP_PATH, "*", "GROUND-TRUTH LABELS", "*FORMAT.npy"))
-        
+        for x in glob.glob(os.path.join(SEAMAP_PATH, "*", "DATA", "*radiance.npy"))
     ]
 
+IMAGE_SCALE = (0, 65535)
+minmax = MinMaxScaler(IMAGE_SCALE)
+
+def scale_vals16(arr):
+        """
+        Scale the images to 16-bits.
+ 
+        Arguments:
+            arr {numpy array} -- images
+ 
+        Returns:
+            numpy array -- 16-bit images
+        """
+        arr_ = arr
+        if arr.ndim > 2:
+            arr_ = arr_.reshape(-1,arr.shape[2])
+
+        arr_ = minmax.fit_transform(arr_)
+        arr_ = arr_.reshape(arr.shape)
+        return arr_.astype(np.uint16)
 
 for file in products:
-    Z = np.load(file)
-    i = file.index("class_NPY_FORMAT.npy")
-    
-    id = file.index("1-DATA WITH GROUND-TRUTH LABELS")
-    id2 = file.index("GROUND-TRUTH LABELS", id+32+1)
-    folder = file[id+32:id2-2]
-    out_file = file[0:id2+20] + folder + 'hypso_gt.tif'
-
+    Z_float = np.load(file)
+    Z = scale_vals16(Z_float)
+    i = file.index(".npy")
+    out_file = file[0:i] + '.tif'
 
     new_dataset = rasterio.open(
         out_file,
@@ -31,10 +47,10 @@ for file in products:
         driver='GTiff',
         height=Z.shape[0],
         width=Z.shape[1],
-        count=1,
+        count=Z.shape[2],
         dtype=Z.dtype
     )
-    
-    img = Z[:, :]
-    new_dataset.write(img, 1)
+    for i in range(Z.shape[2]):
+        img = Z[:, :, i]
+        new_dataset.write(img, i+1)
     new_dataset.close()
--- hypso_data/convert_groundtruth_2tifs.py
+++ hypso_data/convert_groundtruth_2tifs.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 import glob
 import os
+import os.path
 
 SEAMAP_PATH = "C:/Users/akuru/Documents/ECEE 7370 Advanced Comp Vision/sea_cloud_land/1-DATA WITH GROUND-TRUTH LABELS"
 
@@ -20,7 +21,10 @@ for file in products:
     id2 = file.index("GROUND-TRUTH LABELS", id+32+1)
     folder = file[id+32:id2-1]
     out_file = file[0:id2+20] + folder + '-hypso_gt.tif'
+    out_file_TIF = file[0:id2+20] + folder + '-hypso_gt.TIF'
 
+    if (os.path.isfile(out_file_TIF)):
+        os.remove(out_file_TIF)
 
     new_dataset = rasterio.open(
         out_file,
@@ -33,5 +37,7 @@ for file in products:
     )
     
     img = Z[:, :]
+    # convert to 2 labels - ground and clouds
+    new_img = np.where(img<2, 0, img)
     new_dataset.write(img, 1)
     new_dataset.close()
